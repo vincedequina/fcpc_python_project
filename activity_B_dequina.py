b41633888d88ca17bb029562e708888c232b1ba5
LAB3-PC12 <vincedequina>
2018-11-26 19:16:00 -0800

    activity_B

---
+++ activity_B_dequina.py
@@ -0,0 +1,18 @@
+print (1)
+for x in range (8):
+    print (2**x, end ='\t')
+print ()
+print(2)
+for y in range (1,13):
+    if y % 4 == 0:
+        print ('S', end = '\t')
+    else:
+        print (y, end = '\t')
+print()
+print(3)
+for z in range (1 , 9):
+    if z % 8 == 1:
+           print(z , end = "\t")
+    else:
+           print("E", end = "\t")
+print ()
