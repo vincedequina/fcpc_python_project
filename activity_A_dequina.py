8617f4f056697e3489286235429bb6da8bb80155
LAB3-PC12 <vincedequina>
2018-11-26 19:16:10 -0800

    activity_A

---
+++ activity_A_dequina.py
@@ -0,0 +1,15 @@
+print(1)
+for x in range (1,17,3):
+    print (x, end = '\t')
+print()
+print(2)
+for y in range (100,75,-7):
+    print (y, end ='\t')
+print()
+print(3)
+for z in range (1,5):
+    if z % 3 == 0:
+         print(1,end = '\t')
+    else:
+         print(0,end = '\t')
+print()
